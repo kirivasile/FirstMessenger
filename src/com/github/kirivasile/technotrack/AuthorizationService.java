ae5ff237ddf12f3cf67457ff51f2d03c48a14e11
Kirill <kirivasile@yandex.ru>
2015-10-02 21:20:35 +0300

    Реализовано хранение данных в файле, исправлены ошибки.

--- src/com/github/kirivasile/technotrack/AuthorizationService.java
+++ src/com/github/kirivasile/technotrack/AuthorizationService.java
@@ -18,43 +18,63 @@ public class AuthorizationService {
     }
 
     public void run() {
-        String name;
+        String command;
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
-            while (true) {
-                System.out.println("Please enter your name. If you want to exit, type \"EXIT\"");
-                name = reader.readLine();
-                if (name != null) {
-                    if (name.equals("EXIT")) {
-                        break;
-                    }
-                    User currentUser = userStore.getUser(name);
-                    if (currentUser != null) {
-                        String password;
-                        System.out.println("User found. Please enter your password");
-                        password = reader.readLine();
-                        if (currentUser.checkPassword(password)) {
-                            System.out.println("Password is correct");
-                        } else {
-                            System.out.println("Password is incorrect");
-                        }
-                    } else {
-                        System.out.println("Hello, " + name + "! Please enter your password");
-                        String password = reader.readLine();
-                        if (password != null) {
-                            userStore.addUser(new User(name, password));
-                            System.out.println("New user was succesfully added!");
-                        } else {
-                            System.out.println("Incorrect password");
-                        }
-                    }
-                } else {
-                    System.out.println("Incorrect name");
-                }
+            System.out.println("Hello. Do you want to sign up or sign in? Type IN or UP. ");
+            command = reader.readLine();
+            if (command.equals("UP")) {
+                registerUser(reader);
+            } else if (command.equals("IN")) {
+                authorizeUser(reader);
+            } else {
+                System.out.println("Wrong command.");
             }
         } catch (IOException e) {
             System.err.println("IOException in opening reader");
-        } catch (NullPointerException e) {
-            System.out.println("Null pointer exception");
+        } catch (Exception e) {
+            System.out.println("Exception caught: " + e.toString());
+        }
+    }
+
+    private void registerUser(BufferedReader reader) throws IOException {
+        String name;
+        System.out.println("Signing up. Please enter your name.");
+        name = reader.readLine();
+        if (name != null) {
+            User currentUser = userStore.getUser(name);
+            if (currentUser != null) {
+                System.out.println("Sorry, but user with this name has already registered");
+            } else {
+                System.out.println("Please enter your password");
+                String password = reader.readLine();
+                userStore.addUser(new User(name, password));
+                System.out.println("User was successfully signed up");
+            }
+        } else {
+            System.out.println("Incorrect name");
+        }
+    }
+
+    private void authorizeUser(BufferedReader reader) throws IOException {
+        String name;
+        System.out.println("Signing in. Please enter your name.");
+        name = reader.readLine();
+        if (name != null) {
+            User currentUser = userStore.getUser(name);
+            if (currentUser != null) {
+                String password;
+                System.out.println("User found. Please enter your password");
+                password = reader.readLine();
+                if (currentUser.checkPassword(password)) {
+                    System.out.println("Hello, " + name + "!");
+                } else {
+                    System.out.println("Password is incorrect");
+                }
+            } else {
+                System.out.println("Sorry, but we didn't find user with this name: " + name);
+            }
+        } else {
+            System.out.println("Incorrect name");
         }
     }
 }
